87edd998126d79e96a404cf7a083f95675f4aff2
Steve Cirner <scirner@magento.com>
2017-04-18 09:23:57 -0400

    Add new echo to ssh config and add vim plugin for coloring coffeescript.

--- load.sh
+++ load.sh
@@ -39,5 +39,6 @@ ssh core 'cd ~/.vim/bundle && git clone git://github.com/tpope/vim-fugitive.git'
 ssh core 'cd ~/.vim/bundle && git clone git://github.com/scrooloose/nerdtree.git'
 ssh core 'cd ~/.vim/bundle && git clone git://github.com/vim-airline/vim-airline'
 ssh core 'cd ~/.vim/bundle && git clone git://github.com/tpope/vim-surround.git'
+ssh core 'cd ~/.vim/bundle && git clone git://github.com/kchmck/vim-coffee-script.git'
 ssh core 'cd ~/.vim/bundle && git clone https://github.com/vim-scripts/paredit.vim'
 scp $current_dir/vimrc core:~/.vimrc
