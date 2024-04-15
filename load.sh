734e81b3a36c23b9adf59f2334081be2831df444
Stephen Cirner <scirner@provenance.io>
2024-04-15 15:27:48 -0400

    first day at Provenance changes

--- load.sh
+++ load.sh
@@ -11,11 +11,7 @@ current_dir=$(pwd)
 [ -h ~/.ideavimrc ] || ln -s $current_dir/ideavimrc ~/.ideavimrc
 [ -h ~/.config/nvim/init.vim ] || (mkdir -p ~/.config/nvim && ln -s $current_dir/init.vim ~/.config/nvim/init.vim)
 [ -h ~/.gitconfig ] || ln -s $current_dir/gitconfig ~/.gitconfig
-# [ -h ~/.config/tmuxinator/crossbeam.yml ] || (mkdir -p ~/.config/tmuxinator && ln -s $current_dir/tmuxinator/crossbeam.yml ~/.config/tmuxinator/crossbeam.yml)
-[ -h ~/.config/tmuxinator/figure.yml ] || (mkdir -p ~/.config/tmuxinator && ln -s $current_dir/tmuxinator/figure.yml ~/.config/tmuxinator/figure.yml)
-# [ -h ~/.config/tmuxinator/blockchain.yml ] || (mkdir -p ~/.config/tmuxinator && ln -s $current_dir/tmuxinator/blockchain.yml ~/.config/tmuxinator/blockchain.yml)
-# [ -h ~/.config/tmuxinator/p8e.yml ] || (mkdir -p ~/.config/tmuxinator && ln -s $current_dir/tmuxinator/p8e.yml ~/.config/tmuxinator/p8e.yml)
-# [ -h ~/.config/tmuxinator/pay.yml ] || (mkdir -p ~/.config/tmuxinator && ln -s $current_dir/tmuxinator/pay.yml ~/.config/tmuxinator/pay.yml)
+[ -h ~/.config/tmuxinator/provenance.yml ] || (mkdir -p ~/.config/tmuxinator && ln -s $current_dir/tmuxinator/provenance.yml ~/.config/tmuxinator/provenance.yml)
 [ -h ~/.config/alacritty/alacritty.toml ] || (mkdir -p ~/.config/alacritty && ln -s $current_dir/alacritty.toml ~/.config/alacritty/alacritty.toml)
 [ -h ~/.config/fish/config.fish ] || (mkdir -p ~/.config/fish && ln -s $current_dir/config.fish ~/.config/fish/config.fish)
 
@@ -38,9 +34,32 @@ brew install tmuxinator
 brew install font-noto-sans-mono
 brew install kubectx
 brew install node
+brew install jq
+brew install libpq
+brew install wget
+brew install helm
+brew install cmake
+brew install glow
+brew install tfenv
+
+# maybe list
+brew install krew
+# brew install skaffold
+# brew tap GoogleContainerTools/kpt https://github.com/GoogleContainerTools/kpt.git
+# brew install kpt
+
+kubectl krew install neat
+
+brew link --force libpq
+
+# TODO
+# install gh
+# gh extension install k1LoW/gh-grep
 
 mkdir -p ~/.tmux
 [ -d ~/.tmux/kube-tmux ] || git clone https://github.com/jonmosco/kube-tmux ~/.tmux/kube-tmux
 
 # install go
 # curl -s "https://get.sdkman.io" | bash
+# install java
+# install gradle
